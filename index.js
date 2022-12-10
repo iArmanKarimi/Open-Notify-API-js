cb26346bee9855bcd50d3ebff9d5989387416ada
Arman Karimi <mrevid2@gmail.com>
2022-12-11 01:20:29 +0330

    Added api urls

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 const http = require('http'); // server doesn't support https
 
+const URLS = Object.freeze({
+  ASTROS: "http://api.open-notify.org/astros.json",
+  ISS_NOW: "http://api.open-notify.org/iss-now.json",
+})
 
 class OpenNotify {
 }
